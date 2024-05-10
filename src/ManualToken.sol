d72ee28abefbb3c10f8ad1a1c18e05ca15e41809
Matthew Idungafa <mattidungafa@gmail.com>
2024-05-10 16:13:01 +0100

    updates

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -3,6 +3,8 @@
 pragma solidity ^0.8.18;
 
 contract ManualToken {
+    mapping(address => uint256) s_balances; // Mapping to store balances
+
     function name() public pure returns (string memory) {
         return "Manual Token";
     }
@@ -19,7 +21,10 @@ contract ManualToken {
         return s_balances[_owner];
     }
 
-    function transfer(address _to uint256 _amount) public {
-        
+    function transfer(address _to, uint256 _amount) public {
+        uint256 previousBalances = balanceOf(msg.sender) + balanceOf(_to);
+        s_balances[msg.sender] -= _amount;
+        s_balances[_to] += _amount;
+        require(balanceOf(msg.sender) + balanceOf(_to) == previousBalances);
     }
 }
